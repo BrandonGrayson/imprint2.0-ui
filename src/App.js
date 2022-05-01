eecdcb3afdd855beb61352403c4a6c8a8e795910
Brandon <brandonbgrayson@gmail.com>
2022-05-01 03:23:28 -0400

    removed layout class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home';
-import { Box } from "@mui/material"
 
 function App() {
   return (
-    <Box className="layout">
+    
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-    </Box>
+    
   );
 }
 
